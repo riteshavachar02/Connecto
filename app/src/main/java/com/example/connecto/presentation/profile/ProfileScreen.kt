989dc00701a83dff323dda29106e546bf65ce19d
Ritesh Avachar <124338069+riteshavachar02@users.noreply.github.com>
2025-05-26 19:54:04 +0530

    Start implementing ProfileScreen.kt

--- app/src/main/java/com/example/connecto/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/connecto/presentation/profile/ProfileScreen.kt
@@ -1,23 +1,69 @@
 package com.example.connecto.presentation.profile
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
+import com.example.connecto.R
+import com.example.connecto.presentation.components.StandardToolBar
+import com.example.connecto.presentation.profile.components.BannerSection
 
 @Composable
 fun ProfileScreen(
     navController: NavController
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.surface)
     ) {
-        Text(
-            text = "Profile Screen"
+        StandardToolBar(
+            modifier = Modifier.fillMaxWidth(),
+            navController = navController,
+            showArrowBack = false,
+            navActions = {
+                IconButton(
+                    onClick = {}
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = stringResource(id = R.string.more_option)
+                    )
+                }
+            },
+            title = {
+                Text(
+                    text = stringResource(id = R.string.profile),
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            },
         )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+        ) {
+            item() {
+                BannerSection(
+                    modifier = Modifier
+                        .aspectRatio(2.5f),
+                )
+            }
+            items(20) {
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/connecto/presentation/profile/components/BannerSection.kt
@@ -0,0 +1,122 @@
+package com.example.connecto.presentation.profile.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.connecto.R
+import com.example.connecto.presentation.ui.theme.spaceMedium
+import com.example.connecto.presentation.ui.theme.spaceSmall
+import com.example.connecto.presentation.util.toPx
+
+@Composable
+fun BannerSection(
+    modifier: Modifier = Modifier,
+    iconSize: Dp = 35.dp,
+    onGitHubClick: () -> Unit = {},
+    onInstagramClick: () -> Unit = {},
+    onLinkedinClick: () -> Unit = {}
+) {
+    BoxWithConstraints(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.channelart),
+            contentDescription = stringResource(id = R.string.banner_image),
+            modifier = Modifier
+                .fillMaxSize()
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = constraints.maxHeight - iconSize.toPx() * 2f
+                    )
+                )
+        )
+        Row(
+            modifier = Modifier
+                .height(iconSize)
+                .align(Alignment.BottomStart)
+                .padding(spaceSmall)
+        ) {
+            Spacer(modifier = Modifier.width(spaceSmall))
+            Image(
+                painter = painterResource(id = R.drawable.ic_js_logo),
+                contentDescription = "JavaScript Logo",
+                modifier = Modifier.height(iconSize)
+            )
+            Spacer(modifier = Modifier.width(spaceMedium))
+            Image(
+                painter = painterResource(id = R.drawable.ic_csharp_logo),
+                contentDescription = "C# Logo",
+                modifier = Modifier.height(iconSize)
+            )
+            Spacer(modifier = Modifier.width(spaceMedium))
+            Image(
+                painter = painterResource(id = R.drawable.ic_kotlin_logo),
+                contentDescription = "Kotlin Logo",
+                modifier = Modifier.height(iconSize)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .height(iconSize)
+                .align(Alignment.BottomEnd)
+                .padding(spaceSmall)
+        ) {
+            IconButton(
+                onClick = onGitHubClick,
+                modifier = Modifier.size(iconSize)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_github_icon_1),
+                    contentDescription = "GitHub Logo",
+                    modifier = Modifier.size(iconSize)
+                )
+            }
+            IconButton(
+                onClick = onLinkedinClick,
+                modifier = Modifier.size(iconSize)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_linkedin_icon_1),
+                    contentDescription = "Linkedin Logo",
+                    modifier = Modifier.size(iconSize)
+                )
+            }
+            IconButton(
+                onClick = onInstagramClick,
+                modifier = Modifier.size(iconSize)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_instagram_glyph_1),
+                    contentDescription = "Instagram Logo",
+                    modifier = Modifier.size(iconSize)
+                )
+            }
+        }
+    }
+}
