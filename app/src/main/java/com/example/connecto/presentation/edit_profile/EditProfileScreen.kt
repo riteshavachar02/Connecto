c3ed661a1b1db96e7105b7e652a0014c6950e966
Ritesh Avachar <124338069+riteshavachar02@users.noreply.github.com>
2025-05-31 23:20:15 +0530

    Start implementing EditProfileScreen.kt

--- app/src/main/java/com/example/connecto/presentation/edit_profile/EditProfileScreen.kt
+++ app/src/main/java/com/example/connecto/presentation/edit_profile/EditProfileScreen.kt
@@ -1,9 +1,9 @@
 package com.example.connecto.presentation.edit_profile
 
+import MediumGray
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,32 +12,67 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.connecto.R
 import com.example.connecto.presentation.components.StandardTextField
+import com.example.connecto.presentation.components.StandardToolBar
 import com.example.connecto.presentation.ui.theme.profilePictureSizeLarge
 import com.example.connecto.presentation.ui.theme.spaceMedium
+import com.example.connecto.presentation.util.Screen
 
 @Composable
 fun EditProfileScreen(
-    navController: NavController,
-    modifier: Modifier = Modifier
+    navController: NavController
 ) {
     Column(
         modifier = Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Top
+            .fillMaxSize()
+            .background(MediumGray)
     ) {
+        StandardToolBar(
+            navController = navController,
+            title = {
+                Text(
+                    text = stringResource(id = R.string.edit_profile),
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            },
+            modifier = Modifier.fillMaxWidth(),
+            showArrowBack = true,
+            navActions = {
+                IconButton(onClick = {
+                    navController.navigate(Screen.ProfileScreen.route)
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = stringResource(id = R.string.submit),
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MediumGray)
+        ) {  }
         Image(
             painter = painterResource(id = R.drawable.channelart),
             contentDescription = stringResource(id = R.string.banner_image),
@@ -48,7 +83,7 @@ fun EditProfileScreen(
                 )
         )
         Column(
-            modifier = modifier
+            modifier = Modifier
                 .fillMaxWidth()
                 .background(
                     color = MaterialTheme.colorScheme.background
@@ -68,11 +103,11 @@ fun EditProfileScreen(
                         shape = CircleShape
                     )
                     .background(
-                        color = MaterialTheme.colorScheme.background
+                        color = MaterialTheme.colorScheme.surface
                     )
             )
             Box(
-                modifier = modifier
+                modifier = Modifier
                     .padding(spaceMedium)
                     .fillMaxWidth()
                     .background(MaterialTheme.colorScheme.background)
